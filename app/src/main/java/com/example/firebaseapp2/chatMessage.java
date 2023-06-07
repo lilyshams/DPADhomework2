108d89981776399dc029c21d06645992340f6df0
lilyshams <lilyshams23@gmail.com>
2023-06-07 18:42:41 +0300

    first commit

---
+++ app/src/main/java/com/example/firebaseapp2/chatMessage.java
@@ -0,0 +1,15 @@
+package com.example.firebaseapp2;
+
+public class chatMessage {
+    public String userPhoto;
+    public String userName;
+    public String userID;
+    public String message;
+
+    public chatMessage(String userPhoto, String userName, String userID, String message) {
+        this.userPhoto = userPhoto;
+        this.userName = userName;
+        this.userID = userID;
+        this.message = message;
+    }
+}
